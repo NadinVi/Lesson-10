59440e741313029ebf280fb7b610c31cb658ae35
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-13 12:54:55 +0100

    add task23

--- 23/script.js
+++ 23/script.js
@@ -3,3 +3,20 @@
 // Если свойство повторяется, то взять значение второго объекта
 // assignObjects({ x: 10, y: 20 }, { z: 30 }) вывод { x:10, y:20, z: 30 }
 // assignObjects({ x: 10 }, { x: 20, y: 30 }) вывод { x:20, y: 30 }
+
+let firstObj = {
+    x: 10,
+    y: 20,
+};
+
+let secondObj = {
+    y: 30,
+    z: 50,
+    f: 70,
+};
+
+function assignObjects(obj1, obg2) {
+    return Object.assign({}, obj1, obg2)
+}
+
+console.log(assignObjects(firstObj, secondObj));
